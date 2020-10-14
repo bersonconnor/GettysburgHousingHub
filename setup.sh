ca6b962090e4d3e5b9ef6ee316e0e4de7ff38571
bersonconnor <bersonconnor@gmail.com>
2020-10-13 20:29:04 -0400

    Added info and linked landing to info. Added setup.sh file for quick permission changing.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+# Changing directory permissions
+chmod a+rx .
+chmod a+rx ./front_end/
+chmod a+rx ./front_end/images/
+
+# Changing file permissions
+chmod a+r ./front_end/*
+chmod a+r ./front_end/images/*
