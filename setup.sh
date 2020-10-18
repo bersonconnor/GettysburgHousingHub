7fb458233b8a34718ab1ca01cdd2c9cb7287b948
bersonconnor <bersonconnor@gmail.com>
2020-10-18 15:57:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@
 chmod a+rx .
 chmod a+rx ./front_end/
 chmod a+rx ./front_end/images/
+chmod a+rx ./front_end/components/
 
 # Changing file permissions
 chmod a+r ./front_end/*
 chmod a+r ./front_end/images/*
+chmod a+r ./front_end/components/*
