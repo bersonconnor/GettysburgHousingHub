66d5523477dd11cc7d1537ac09a9e4b58a1dfdeb
hochtr01 <hochtr01@gettysburg.edu>
2020-10-20 22:37:46 -0400

    Added links and phone numbers to res-life

--- front_end/info.php
+++ front_end/info.php
@@ -15,7 +15,37 @@
     <div class="col-sm-12">
       <h2>RESIDENTIAL EDUCATION</h2><br>
       <p> At the heart of the mission of Gettysburg College is the fact that we are a residential campus. Gettysburg offers a wide variety of housing options that are designed to be highly supportive and interactive in the first two years and move toward independent living in the last two years. We are committed to developing a caring community that encourages academic success, champions individual respect, and demands responsibility within our community. In order to achieve these community ideals, we expect all residents to work together to create a positive living environment.</p>
-    </div>
+
+	<br> 
+	<form action="https://www.gettysburg.edu/offices/residential-first-year-programs">
+	    <input type="submit"  value="Gettysburg Housing Website" />
+	</form>
+
+	<div class="gb-c-contact gb-u-bg-color-alabaster">
+		<h3 class="gb-c-contact__heading gb-u-type-epsilon gb-u-line-height">Contact</h3>
+		<h4 class="gb-c-contact__subheading gb-u-type-zeta">Address</h4>
+		<div class="gb-c-contact__body gb-u-type-p--caption">
+		Residential &amp; First-Year Programs<br>	
+		Campus Box 430	
+		<br>		
+		CUB 250
+		<br>		
+		300 N. Washington Street<br>Gettysburg,PA 17325
+		</div>
+		<h4 class="gb-c-contact__subheading gb-u-type-zeta">Email</h4>
+		<div class="gb-c-contact__body gb-u-type-p--caption">	
+		<a href="mailto:residencelife@gettysburg.edu">residencelife@gettysburg.edu</a>
+		</div>					
+		<h4 class="gb-c-contact__subheading gb-u-type-zeta">Phone</h4>
+		<div class="gb-c-contact__body gb-u-type-p--caption"
+		<a href="tel:+1-(717)337-6901">(717)337-6901</a>	
+		</div>
+		<h4 class="gb-c-contact__subheading gb-u-type-zeta">Fax</h4>
+		<div class="gb-c-contact__body gb-u-type-p--caption">
+		<a href="tel:+1-(717)337-6335">(717)337-6335</a>
+		</div>	
+	</div>	
+  </div>
   </div>
 </div>
 
@@ -23,8 +53,8 @@
   <div class="row">
     <div class="col-sm-12">
       <h2>IMPORTANT DATES</h2><br>
-      <p>August 21   -    First-year residence halls open
-	August 23   -    Upperclass residence halls open
+      <p>August 21   -    First-year residence halls open<br>
+	August 23   -    Upperclass residence halls open<br>
 	August 26   -    First day of classes</p>
     </div>
   </div>
