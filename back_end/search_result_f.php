63c861da1ad3e204586eca2485f33e39e4e3cede
bersonconnor <bersonconnor@gmail.com>
2020-10-26 16:49:36 -0400

    Implemented functionality for searching the housing database. The front end work still needs to be completed.

--- back_end/search_result_f.php
+++ back_end/search_result_f.php
@@ -1,45 +1,74 @@
 <?php
-	include_once("db_connect.php");
+	/*
+	* Prints out the table of results when the user searches the housing database.
+	* The parameters the user searches by are extracted from $_POST.
+	*/
+	function search() {
+		// TODO: MAKE IT SO INCLUDING OF db_connect.php FUNCTIONS
+		// RIGHT NOW IT ONLY FUNCTIONS IF IN CODE METHOD
+                $host="ada.cc.gettysburg.edu";
+                $dbase="f20_3";
+                $user="bersco01";
+                $pass="bersco01";
 
-	/* Get search variables from $_POST */
-	$name = $_POST["name"];
-	$cap  = $_POST["capacity"];
-	$kit  = $_POST["kitchen"];
-	$park = $_POST["parking"];
-	print "<p>$name\n$cap\n$kit\n$park\n</p><br />";
+                try {
+                        // PHP Database Object
+                        $db = new PDO("mysql:host=$host;dbname=$dbase", $user, $pass);
+                }
+                catch (PDOException $e) {
+                        // . is the concatenation operator
+                        die("Error connecting to mysql database " . $e->getMessage());
+                }
 
-	/* Choose the appropriate query depending on user selection*/
-	$q = "";
-	if ($name != 'NULL' && $cap != 'NULL') {
-		$q = "SELECT Name, Number, Size, FloorPlan FROM BUILDING JOIN ROOM ON Name=BuildingName WHERE Name='$name' AND Size='$cap';";
-	}
-	else if($name != 'NULL' && $cap == 'NULL') {
-		$q = "SELECT Name, Number, Size, FloorPlan FROM BUILDING JOIN ROOM ON Name=BuildingName WHERE Name='$name';";
-	}
-	else if ($name == 'NULL' && $cap != 'NULL') {
-		$q = "SELECT Name, Number, Size, FloorPlan FROM BUILDING JOIN ROOM ON Name=BuildingName WHERE Size='$cap';";
-	}
-	else {
-		$q = "SELECT Name, Number, Size, FloorPlan FROM BUILDING JOIN ROOM ON Name=BuildingName;";
-	}
 
-	/* If the query is valid */
-	$res_q = $db->query($q);
-	if ($res_q == TRUE) {
-	        print "<!DOCTYPE html><html><head><title>Search Tester</title></head><body>";
-	        print "<table><tr><td>Building</td><td>Room Number</td><td>Capacity</td><td>Floor Plan</td></tr>";
+		/* Write out the base query */
+		$q = "SELECT Name, Number, Size, FloorPlan FROM BUILDING JOIN ROOM ON Name=BuildingName";
+		$params = "";
+		$n = count($_POST);
+		$keys = array_keys($_POST);
 
-		/* Print every search result that matches the user input */
-		while ($row=$res_q->fetch()) {
-			$building 	= $row['Name'];
-			$number 	= $row['Number'];
-			$size		= $row['Size'];
-			$floor_plan	= $row['FloorPlan'];
-			print "<tr><td>$building</td><td>$number</td><td>$size</td><td>$floor_plan</td></tr>";
+		/* Append all none empty parameters */
+		for ($i = 0; $i < $n; ++$i) {
+	                $key = $keys[$i];
+	                $value = $_POST[$key];
+
+			/* If the current key is not search as that is the button and is not a default value */
+			if ($value !== '' && $key !== 'search') {
+				if ($i !== 0 && $i !== $n - 1) {
+					$params .= " AND ";
+				}
+				$params .= "$key='$value'";
+			}
 		}
+
+		/* If we have any parameters, then we create a WHERe statement */
+		if ($params !== "") {
+		        $q .= " WHERE " . $params;
+		}
+		$q .= ";";
+
+
+		// TODO: PRINTING NEEDS TO BE CHANGED SO THAT IT PRINTS FORMATTED AND STYLIZED RESULTS
+
+
+		/* If the query is valid */
+		$res_q = $db->query($q);
+		if ($res_q == TRUE) {
+		        //print "<!DOCTYPE html><html><head><title>Search Tester</title></head><body>";
+		        print "<table><tr><td>Building</td><td>Room Number</td><td>Capacity</td><td>Floor Plan</td></tr>";
+
+			/* Print every search result that matches the user input */
+			while ($row=$res_q->fetch()) {
+				$building 	= $row['Name'];
+				$number 	= $row['Number'];
+				$size		= $row['Size'];
+				$floor_plan	= $row['FloorPlan'];
+				print "<tr><td>$building</td><td>$number</td><td>$size</td><td>$floor_plan</td></tr>";
+			}
+		}
+		else {
+			print"<p>ERROR</p>";
+		}
+		print "</table>";
 	}
-	else {
-		print"<p>ERROR</p>";
-	}
-	print "</table></body></html>";
 ?>
--- front_end/search.php
+++ front_end/search.php
@@ -3,7 +3,6 @@
 <head>
 	<?php
 		include_once("components/header.html");
-		include_once("../back_end/search_result_f.php");
 	?>
 	<link href="./components/search.css" rel="stylesheet" type="text/css"></link>
 
@@ -16,7 +15,7 @@
 ?>
 
 <section class = "table_wrap"><!-- table_wrap-->
-  <div class = "table_header"><!-- table_heard-->
+  <div class = "table_header"><!-- table_header-->
     <ul>
     <li>
       <div class = "options"><!-- options-->
@@ -43,16 +42,27 @@
     </li>
     </ul>
 
-  </div> <!-- table_heard-->
+  </div> <!-- table_header-->
+
+  <!-- TODO: THE RESULTS THAT ARE PRINTED HERE NEED TO BE FORMATTED AND STYLED -->
+  <!-- TODO: PANE WITH SCROLL BAR NEEDS TO BE ADDED FOR OVERFLOWING RESULTS -->
+  <div class="table_header">
+  <?php
+        include_once("../back_end/search_result_f.php");
+        if(array_key_exists('search', $_POST)) {
+                search();
+        }
+  ?>
+  </div>
 
 <aside>
   <div class="filter_bar"><!-- filter_bar-->
-    <form name="search" method="POST" action="../back_end/search_result_f.php">
-	    <!-- Drop down lists need to be styled -->
+    <form name="search_form" method="POST" <!--action="../back_end/search_result_f.php" --> >
+	    <!-- TODO: DROP DOWNS NEED TO BE STYLED TO MATCH REST OF PAGE -->
 	    <div class="select_building">
 	    <p>Location/Building</p>
-	    <select name="name" style="color:black;width:100%;">
-	       <option value="NULL" >Choose...</option>
+	    <select name="Name" style="color:black;width:100%;">
+	       <option value="" >Choose...</option>
 	       <?php include_once("../back_end/fill_location_f.php"); ?>
 	    </select>
 	    </div>
@@ -60,8 +70,8 @@
 
 	    <div class = "select_capacity">
 	    <p>Room Capacity</p>
-	    <select name="capacity" style="color:black;width:100%;">
-	      <option value="NULL">Choose...</option>
+	    <select name="Size" style="color:black;width:100%;">
+	      <option value="">Choose...</option>
 	      <?php include_once("../back_end/fill_capacity_f.php"); ?>
 	    </select>
 	    </div>
@@ -69,8 +79,8 @@
 
 	    <div class = "select_kitchen">
 	    <p>Kitchen</p>
-	    <select name="kitchen" style="color:black;width:100%;">
-	      <option value="Either" >Either</option>
+	    <select name="Kitchen" style="color:black;width:100%;">
+	      <option value="">Either</option>
 	      <option value = "Yes">Yes</option>
 	      <option value = "No">No</option>
 	    </select>
@@ -79,16 +89,16 @@
 
 	    <div class = "select_parking">
 	    <p>Parking</p>
-	    <select name="parking" style="color:black;width:100%;">
-	      <option value="Either" >Either</option>
+	    <select name="Parking" style="color:black;width:100%;">
+	      <option value="">Either</option>
 	      <option value = "Yes">Yes</option>
 	      <option value = "No">No</option>
 	    </select>
 	    </div>
 	    </br>
 
-	    <!-- Styling needs to be moved to a css file -->
-	    <input type="submit" value+"Search" id="search_button"
+	    <!-- TODO: BUTTON STYLING NEEDS TO BE FINALIZED AND MOVED TO CSS FILE -->
+	    <input type="submit" name="search" id="search" value="Search" id="search_button"
 						style="background-color: #f1eded;
 						border: none;
 						color: black;
@@ -100,47 +110,6 @@
 						width:100%;
 						border-radius: 4px;" />
     </form>
-    <!-- <div class = "select_capacity">
-      <p>Room Capacity:</p>
-      <input type="checkbox" id = "1" name="capacity1" value="1"/>
-      <label for="1">1</label>
-      <input type="checkbox" id = "2" name="capacity2" value="2"/>
-      <label for="2">2</label>
-      <input type="checkbox" id = "3" name="capacity3" value="3"/>
-      <label for="3">3</label>
-      <br>
-      <input type="checkbox" id = "4" name="capacity4" value="4"/>
-      <label for="3">4</label>
-      <input type="checkbox" id = "5" name="capacity5" value="5"/>
-      <label for="3">5</label>
-      <input type="checkbox" id = "6" name="scapacity6" value="6"/>
-      <label for="3">6</label>
-      <br>
-    </div>
-
-    <div class = "select_kitchen">
-      <p>Kitchen:</p>
-      <input type="radio" id="kitchen" name="kt" value="yes">
-      <label for="kitchen">Yes</label>
-      <input type="radio" id="no_kitchen" name="kt" value="no">
-      <label for="kitchen">No</label>
-      <br>
-      <input type="radio" id="either" name="kt" value="either" checked="checked">
-      <label for="kitchen">Either</label>
-      <br>
-    </div>
-
-    <div class = "select_parking">
-      <p>Parking:</p>
-      <input type="radio" id="parking" name="pk" value="yes">
-      <label for="kitchen">Yes</label>
-      <input type="radio" id="no_parking" name="pk" value="no">
-      <label for="kitchen">No</label>
-      <br>
-      <input type="radio" id="either" name="pk" value="either" checked="checked">
-      <label for="kitchen">Either</label>
-      <br>
-    </div> -->
 
   </div><!-- filter_bar-->
 </aside>
