7e0d48e8489ae10910b693deceb7e20cdb0e9a40
Hao Lin <linha01@gettysburg.edu>
2020-10-28 02:15:24 -0400

    search page edits

--- front_end/search.php
+++ front_end/search.php
@@ -4,7 +4,8 @@
 	<?php
 		include_once("components/header.html");
 	?>
-	<link href="./components/search.css" rel="stylesheet" type="text/css"></link>
+ 
+<link href="./components/search.css" rel="stylesheet" type="text/css"></link>
 
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
@@ -14,54 +15,42 @@
 	include_once("components/title.html");
 ?>
 
-<section class = "table_wrap"><!-- table_wrap-->
-  <div class = "table_header"><!-- table_header-->
-    <ul>
-    <li>
-      <div class = "options"><!-- options-->
-        <div class = "t_building">
-        <span> Location/Building </span>
-        </div>
-
-        <div class = "t_room">
-        <span> Room Number </span>
-        </div>
-
-        <div class = "t_capacity">
-        <span> Capacity </span>
-        </div>
-
-        <div class = "t_floor_plan">
-        <span> Floor Plan </span>
-        </div>
-
-        <div class = "t_status">
-        <span> Status </span>
-        </div>
-      </div><!-- options-->
-    </li>
-    </ul>
-
-  </div> <!-- table_header-->
+<section class = "content"><!-- content-->
+  <div class = "search_body"><!-- search-->
+  <table class = "table table-striped table-hover table-bordered"><!-- table-->
+    <thead>
+    <tr>
+      <th scope="col">Location/Building</th>
+      <th scope="col">Room#</th>
+      <th scope="col">Capacity</th>
+      <th scope="col">Floor Plan</th>
+      <th scope="col">Status</th>
+    </tr>
+    </thead>
 
   <!-- TODO: THE RESULTS THAT ARE PRINTED HERE NEED TO BE FORMATTED AND STYLED -->
   <!-- TODO: PANE WITH SCROLL BAR NEEDS TO BE ADDED FOR OVERFLOWING RESULTS -->
-  <div class="table_header">
-  <?php
+  <tbody>
+    <?php
         include_once("../back_end/search_result_f.php");
         if(array_key_exists('search', $_POST)) {
                 search();
         }
   ?>
+  
+  <!-- FOR EVERY ROW IN THE RESULT RESULT, CREATE A ROW -->
+  </tbody>
+  
+  </table>
   </div>
-
+  
 <aside>
   <div class="filter_bar"><!-- filter_bar-->
-    <form name="search_form" method="POST" <!--action="../back_end/search_result_f.php" --> >
+    <form name="search_form" method="POST" <!--action="../back_end/search_result_f.php" -->
 	    <!-- TODO: DROP DOWNS NEED TO BE STYLED TO MATCH REST OF PAGE -->
 	    <div class="select_building">
 	    <p>Location/Building</p>
-	    <select name="Name" style="color:black;width:100%;">
+	    <select name="building" style="color:black;width:100%;">
 	       <option value="" >Choose...</option>
 	       <?php include_once("../back_end/fill_location_f.php"); ?>
 	    </select>
@@ -70,16 +59,16 @@
 
 	    <div class = "select_capacity">
 	    <p>Room Capacity</p>
-	    <select name="Size" style="color:black;width:100%;">
+	    <select name="size" style="color:black;width:100%;">
 	      <option value="">Choose...</option>
 	      <?php include_once("../back_end/fill_capacity_f.php"); ?>
 	    </select>
 	    </div>
 	    </br>
-
+         
 	    <div class = "select_kitchen">
 	    <p>Kitchen</p>
-	    <select name="Kitchen" style="color:black;width:100%;">
+	    <select name="kitchen" style="color:black;width:100%;">
 	      <option value="">Either</option>
 	      <option value = "Yes">Yes</option>
 	      <option value = "No">No</option>
@@ -89,14 +78,61 @@
 
 	    <div class = "select_parking">
 	    <p>Parking</p>
-	    <select name="Parking" style="color:black;width:100%;">
+	    <select name="parking" style="color:black;width:100%;">
 	      <option value="">Either</option>
 	      <option value = "Yes">Yes</option>
 	      <option value = "No">No</option>
 	    </select>
 	    </div>
 	    </br>
+      
+     <button class="btn" type="button" data-toggle="collapse" data-target = "#expand" style="color:black;width:80%;">More</button></br>
+     
+     <div class="collapse" id="expand">
+     <div class = more>
+        <div class = "select_room">
+  	    <p>Room#</p>
+  	    <select name="room" style="color:black;width:100%;">
+  	      <option value="">Choose...</option>
+  	      <?php include_once("../back_end/fill_capacity_f.php"); ?>
+  	    </select>
+  	    </div>
+  	    </br>
+        
+        <div class = "select_bathroom">
+  	    <p>Private Bathroom</p>
+  	    <select name="bathroom" style="color:black;width:100%;">
+  	      <option value="">Either</option>
+  	      <option value = "Yes">Yes</option>
+  	      <option value = "No">No</option>
+  	    </select>
+  	    </div>
+  	    </br>
+           
+        <div class = "select_price">
+  	    <p>Price</p>
+  	    <select name="price" style="color:black;width:100%;">
+  	      <option value="">Choose...</option>
+  	      <?php include_once("../back_end/fill_capacity_f.php"); ?>
+  	    </select>
+  	    </select>
+  	    </div>
+  	    </br>
+        
+        <div class = "select_availability">
+  	    <p>Availability</p>
+  	    <select name="availability" style="color:black;width:100%;">
+  	      <option value="">Either</option>
+  	      <option value = "Yes">Yes</option>
+  	      <option value = "No">No</option>
+  	    </select>
+  	    </div>
+  	            
+      </div>     
+      </div>
+               
 
+      <br>  
 	    <!-- TODO: BUTTON STYLING NEEDS TO BE FINALIZED AND MOVED TO CSS FILE -->
 	    <input type="submit" name="search" id="search" value="Search" id="search_button"
 						style="background-color: #f1eded;
@@ -113,7 +149,8 @@
 
   </div><!-- filter_bar-->
 </aside>
-</section> <!-- table_wrap-->
+
+</section> <!-- content-->
 
 
 <?php
