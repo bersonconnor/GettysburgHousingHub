37be3f1f18a107687fcee8a581c8aef07e5c1f18
hochtr01 <hochtr01@gettysburg.edu>
2020-10-28 23:42:03 -0400

    adds names to drop down menu for admin search

---
+++ back_end/names_f.php
@@ -0,0 +1,27 @@
+<?php
+       /*
+	* Fills the drop down list with all of the students names
+        * in the database.
+        */
+
+        include_once("db_connect.php");
+
+        // Select names ordered (lastname, firstname) in ascending order
+        $query = "SELECT CONCAT(Lname, ' ', Fname) AS Name FROM STUDENT ORDER BY Name ASC;";
+        $res = $db->query($query);
+
+	print("<option value=''>Count($res)</option>");
+        if ($res != TRUE) {
+		// Iterate over all the capacities
+                while ($row = $res->fetch()) {
+                        $name = $row['Name'];
+                        // Inject capacity as HTML option for a drop down
+                        print("<option value='$name'>$name</option>");
+                }
+        }
+        else {
+                print("<option value=''>Error</option>");
+        }
+?>
+
+
