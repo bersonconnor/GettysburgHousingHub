822cc523d246e66796642c0b402a594fda61f4b6
Hao Lin <linha01@gettysburg.edu>
2020-11-03 23:30:12 -0500

    tested all functions

--- back_end/group_f.php
+++ back_end/group_f.php
@@ -8,15 +8,14 @@ function checkGroup($db, $login){
   $group = "SELECT GroupID FROM STUDENT WHERE '$login' = Login";
   $q = $db->query($group);
   
-  
-  if($q != FALSE){
+   if($q != FALSE){
     $result = $q->fetch();
     $g = $result['GroupID'];
-    //The user is not in a group yet
+    //The user is group yet
     if($g != NULL){
       return 1;
     }
-    //The user is in a group
+    //The user is not in a group
     else{
       return -1;
     }
@@ -26,48 +25,71 @@ function checkGroup($db, $login){
   }
 }
 
+
 //Create a group with user himself as the member; 
 
 function createGroup($db, $login){
-  $gId = "SELECT MAX(GroupID) FROM RoomGroup";
-  $q1 = $db->query($id);
-  
+  $gId = "SELECT MAX(GroupID)AS value FROM ROOM_GROUP";
+  $q1 = $db->query($gId);
+  $row1 = $q1->fetch();
+  $curr_ID = $row1['value'];
+  $curr_ID = $curr_ID + 1;
+ 
   $sId = "SELECT ID FROM STUDENT WHERE '$login' = Login";
-  $q2 = $db->query($sID);
+  $q2 = $db->query($sId);
+  $row2 = $q2->fetch();
+  $student_ID = $row2['ID'];
   
-  $create = "INSERT INTO ROOM_GROUP VALUE ($gId + 1, $sId, 1);";
+  $create = "INSERT INTO ROOM_GROUP VALUES ($curr_ID, $student_ID, 1)";
   $q3 = $db->query($create);
   
-  $update = "UPDATE STUDENT SET GroupID = $gID WHERE '$login' = Login;";
+  $update = "UPDATE STUDENT SET GroupID = $curr_ID WHERE '$login' = Login;";
   $q4 = $db->query($update);
   
   if( $q1 != FALSE && $q2 != FALSE && $q3 != FALSE && $q4 != FALSE){
-    return TRUE;
-  }
+    header("Location: ../front_end/group.php");
+   }
   else{
     print"<p>ERROR</p>";
   }
 }
 
-/*
-// Leave a group
+//Leave a group
 function leaveGroup($db, $login){
 
-  $leave = "UPDATE STUDENT SET GroupID = NULL '$login' = Login";
-  $q = $db->query($leave);
+  $gId = "SELECT GroupID FROM STUDENT WHERE '$login' = Login";
+  $q1 = $db->query($gId);
+  $row1 = $q1->fetch();
+  $curr_ID = $row1['GroupID'];
   
-  if( $q != FALSE ){
-    return TRUE;
+  $gSize = "SELECT Size FROM ROOM_GROUP WHERE GroupID = $curr_ID";
+  $q2 = $db->query($gSize);
+  $row2 = $q2->fetch();
+  $curr_Size = $row2['Size'];
+  
+  
+  $leave = "UPDATE STUDENT SET GroupID = NULL WHERE Login ='$login' ";
+  $q3 = $db->query($leave);
+
+  if($q1 != FALSE && $q2 != FALSE && $q3 != FALSE){
+    if($curr_Size == 1){
+      $clear_group = "DELETE FROM ROOM_GROUP WHERE GroupID = $curr_ID";
+      $q4 = $db->query($clear_group);
+      
+      if($q4 == FALSE){
+        print"<p> q4 ERROR</p>";
+      }
+      else{
+        header("Location: ../front_end/group.php");
+      }
+    }
   }
   else{
     print"<p>ERROR</p>";
   }
 }
-*/
 
 //Show all the group member of the user
-
-
 function showGroup($db, $login){
   $group = "SELECT GroupID, Fname, Lname FROM STUDENT NATURAL JOIN  ROOM_GROUP
                 WHERE GroupID = (SELECT t2.GroupID FROM STUDENT AS t2 WHERE '$login' = Login)";
@@ -90,7 +112,7 @@ function showGroup($db, $login){
 					echo "<div class='row' style='background: #f2f2f2; width: 50%;'>";
 				}
 
-				/* Print each variable */
+				// Print each variable 
 				foreach ($arr as $val) {
 					echo "<div class='col-md-2'><p style='color: black;'>$val</p></div>";
 				}
